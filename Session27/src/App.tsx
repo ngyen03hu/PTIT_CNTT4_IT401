fc598155cc92bf214fd042d0c8252bdab65f0634
ducthinhit <157384683+ducthinhit@users.noreply.github.com>
2025-09-12 09:52:30 +0700

    SS27

--- Session27/src/App.tsx
+++ Session27/src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Ex01 from './Components/PTIT_CNTT4_IT104_Session27_Exercises01/Ex01'
 import Ex02 from './Components/PTIT_CNTT4_IT104_Session27_Exercises02/Ex02'
 import Ex03 from './Components/PTIT_CNTT4_IT104_Session27_Exercises03/Ex03'
+import Ex04 from './Components/PTIT_CNTT4_IT104_Session27_Exercises04/Ex04'
+import Ex05 from './Components/PTIT_CNTT4_IT104_Session27_Exercises05/Ex05'
+import Ex06 from './Components/PTIT_CNTT4_IT104_Session27_Exercises06/Ex06'
+import Ex07 from './Components/PTIT_CNTT4_IT104_Session27_Exercises07/Ex07'
+import Ex08 from './Components/PTIT_CNTT4_IT104_Session27_Exercises08/Ex08'
 
 export default function App() {
   return (
@@ -9,6 +14,11 @@ export default function App() {
       {/* <Ex01 /> */}
       {/* <Ex02 /> */}
       {/* <Ex03></Ex03> */}
+      {/* <Ex04 />
+      <Ex05 />
+      <Ex06 />
+      <Ex07 />
+      < Ex08 /> */}
     </div>
   )
 }
---
+++ Session27/src/Components/PTIT_CNTT4_IT104_Session27_Exercises05/Ex05.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./ProductList";
+export default function Ex05() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/products" element={<ProductList />} />
+            </Routes>
+        </Router>
+    );
+}
